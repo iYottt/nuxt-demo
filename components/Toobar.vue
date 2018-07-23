bb91b2b6bb65b8277dee86f6944f8ce55c57fb46
Yotsakon <iyottt@gmail.com>
2018-07-23 16:28:46 +0700

    add all files

---
+++ components/Toobar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-toolbar dark color="blue">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-toolbar-title class="white--text">
+        <nuxt-link class="title" to="/">Devda</nuxt-link>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<style scoped>
+.title {
+    text-decoration: none !important;
+}
+.title:visited {
+    color: white;
+}
+</style>
